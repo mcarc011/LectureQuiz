cf836e5292cd60c59953f03f112d0f7b91c4975c
mcarc011 <mario.carcamo05@gmail.com>
2025-02-03 21:53:58 -0500

    savess

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def check_answer(user_answer, correct_answer):
 def magnitude_and_angle(vector):
     magnitude = np.linalg.norm(vector)
     angle = np.degrees(np.arctan2(vector[1], vector[0]))
-    return round(float(magnitude),2), round(float(angle),2)
+    mag,angle = round(float(magnitude),2), round(float(angle),2)
+    return 'You walked '+str(mag)+' miles at an angle of ' + str(angle) + ' degrees'
 
 # Streamlit app
 st.title("Vector Addition")
